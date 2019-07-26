9e32b0b7edc3f3a175aad8e4914fa10251f9860a
ProfesorCIFP <cjgardia.cifp@gmail.com>
2019-07-26 18:22:20 +0100

    Ejemplo de uso de ListView recogiendo los datos de un ArrayList

--- app/src/main/java/com/example/a09uilistview/MainActivity.java
+++ app/src/main/java/com/example/a09uilistview/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvDiasSemana = null;
@@ -22,20 +24,38 @@ public class MainActivity extends AppCompatActivity {
         lvDiasSemana = findViewById(R.id.lvDiasSemana);
         txSeleccion = findViewById(R.id.txSeleccion);
 
-        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this,
-                                R.array.diasSemana,
-                                R.layout.support_simple_spinner_dropdown_item);
+        ArrayList<String> diasSemana = creaDiasSemana();
+//        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this,
+//                                R.array.diasSemana,
+//                                R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter adaptador = new ArrayAdapter(getApplicationContext(),
+                                    R.layout.support_simple_spinner_dropdown_item,
+                                    diasSemana);
+
 
         lvDiasSemana.setAdapter(adaptador);
 
         lvDiasSemana.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l) {
-                if (posicion != 0)
-                    txSeleccion.setText("Selección: " + adapterView.getItemAtPosition(posicion));
+                txSeleccion.setText("Selección: " + adapterView.getItemAtPosition(posicion));
 
             }
         });
 
+    }
+
+    private ArrayList<String> creaDiasSemana() {
+        ArrayList<String> diasSemana = new ArrayList<>();
+
+        diasSemana.add("Lunes");
+        diasSemana.add("Martes");
+        diasSemana.add("Miércoles");
+        diasSemana.add("Jueves");
+        diasSemana.add("Viernes");
+        diasSemana.add("Sabado");
+        diasSemana.add("Domingo");
+
+        return diasSemana;
     }
 }
